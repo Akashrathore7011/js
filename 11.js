d1dd4558cef9b7aa4492aece6085996de3c7e482
Akashrathore7011 <akkurathore3250@gmail.com>
2021-05-13 13:08:01 +0530

    first commit

---
+++ 11.js
@@ -0,0 +1,102 @@
+// class divControl{
+//     mw= "0px";
+//     mh="0px";
+//     mf = "0px";
+//     mb = ""
+//     id = "#";
+
+//       addcumb(){
+//           this.divs = document.getElementById(this.id);
+//           this.divs.style.width = this.mw;
+//           this.divs.style.height = this.mh;
+//           this.divs.style.fontSize = this.mf;
+//           this.divs.style.background = this.mb;    
+//         }
+//       } 
+           
+
+//           let akash = new divControl();
+//           akash.id = "A";
+//           akash.mw = "280px";
+//           akash.mh = "60px";
+//           akash.mf = "30px";
+//           akash.mb = "red"   
+//            akash.addcumb();
+          
+          
+          
+//           let najir = new divControl();
+//           najir.id = "B";
+//           najir.mw = "290px";
+//           najir.mh = "80px";
+//           najir.mf = "30px";
+//           najir.mb = "green"
+//              najir.addcumb();
+
+
+        
+          
+        //   console.log(this.divs);
+
+// let divs = new abcd();
+// school.addcumb();
+// let school2 = new abcd();
+// school2.id="pankaj";
+// let school3 = new abcd();
+// school3.id="ankit";
+// let school_4 = new abcd();
+// school_4.id="riya";
+// let school_5 = new abcd();
+// school_5.id="dhruv";
+// school2.addcumb();
+// school3.addcumb();
+// school_4.addcumb();
+// school_5.addcumb();
+
+
+class divControl{
+   w= "0px";
+   h = "0px";
+   f = "";
+   b = "";
+   bgc = "";
+   id = "";
+
+    addfun(){
+       this.divs = document.getElementById(this.id);
+       this.divs.style.width = this.w;
+       this.divs.style.height = this.h;
+       this.divs.style.fontSize = this.f;
+       this.divs.style.border = this.b;
+       this.divs.style.background = this.bgc;
+    }
+  }
+
+     akash = new divControl();
+     akash.id ="A";
+     akash.w = "200px";
+     akash.h = "100px";
+     akash.b = "2px solid black";
+     akash.bgc = "red";
+     akash.addfun();
+
+     akash = new divControl();
+     akash.id ="B";
+     akash.w = "200px";
+     akash.h = "100px";
+     akash.b = "2px solid black";
+     akash.bgc = "gray";
+     akash.addfun();
+     
+     akash = new divControl();
+     akash.id ="C";
+     akash.w = "200px";
+     akash.h = "100px";
+     akash.b = "2px solid black";
+     akash.bgc = "green";
+     akash.addfun();
+     
+
+
+
+
